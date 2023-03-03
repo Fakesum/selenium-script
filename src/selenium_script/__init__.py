5a47985fb320dba827003d8a545399741bf53457
Fakesum <worldanvilbild@gmail.com>
2023-03-03 20:01:07 +0530

    Initial Fix

--- src/selenium_script/__init__.py
+++ src/selenium_script/__init__.py
@@ -1,6 +1,50 @@
-__version__ = "0.0.1b"
+__version__ = "0.0.3c"
 
 from seleniumbase import SB, BaseCase
+
+def _parse_command(command, *script_args):
+    # Split Line into Command and argument
+    command, *raw_args = command.split(" ")
+
+    args = []
+
+    is_middle_of_string = False
+    new_arg_buffer = ""
+    
+    for arg in raw_args:
+        # If multiple arguments are in a single string
+        # they are considered as the same argument
+
+        # If start of string
+        if arg.startswith('"') or arg.startswith("'"):
+            new_arg_buffer += arg
+            is_middle_of_string = True
+        
+        # If middle of string
+        elif is_middle_of_string:
+            new_arg_buffer += arg
+        
+        # If end of string
+        elif arg.endswith('"') or arg.endswith("'"):
+            is_middle_of_string = False
+            new_arg_buffer += arg
+            for script_arg in script_args:
+                new_arg_buffer = new_arg_buffer.replace(f"""[[ {script_arg["key"]} ]]""", script_arg["value"])
+            args.append(eval(new_arg_buffer))
+            new_arg_buffer = ""
+        
+        # If not part of a string
+        else:
+            for script_arg in script_args:
+                arg = arg.replace(f"""[[ {script_arg["key"]} ]]""", script_arg["value"])
+            args.append(eval(arg))
+    
+    # store the commands and argument 
+    # that are executed later
+    return {
+        "command": command,
+        "args": args
+    }
 
 def _parse_commands(raw_commands, *script_args):
     """
@@ -8,51 +52,13 @@ def _parse_commands(raw_commands, *script_args):
     """
     # Parse Arguments and commands
     commands = []
+
     for command in raw_commands:
-        # Split Line into Command and argument
-        command, *raw_args = command.split(" ")
-
-        args = []
-
-        is_middle_of_string = False
-        new_arg_buffer = ""
-        
-        for arg in raw_args:
-            # If multiple arguments are in a single string
-            # they are considered as the same argument
-
-            # If start of string
-            if arg.startswith('"') or arg.startswith("'"):
-                new_arg_buffer += arg
-                is_middle_of_string = True
-            
-            # If middle of string
-            elif is_middle_of_string:
-                new_arg_buffer += arg
-            
-            # If end of string
-            elif arg.endswith('"') or arg.endswith("'"):
-                is_middle_of_string = False
-                new_arg_buffer += arg
-                for script_arg in script_args:
-                    new_arg_buffer = new_arg_buffer.replace(f"""[[ {script_arg["key"]} ]]""", script_arg["value"])
-                args.append(eval(new_arg_buffer))
-                new_arg_buffer = ""
-            
-            # If not part of a string
-            else:
-                for script_arg in script_args:
-                    arg = arg.replace(f"""[[ {script_arg["key"]} ]]""", script_arg["value"])
-                args.append(eval(arg))
-        
-        # store the commands and argument 
-        # that are executed later
-        commands.append({
-            "command": command,
-            "args": args
-        })
+        commands.append(_parse_command(command))
     return commands
-def run_selenium_script(*script_args, name: str = None, uc: bool | None = None):
+
+
+def run_selenium_script(*script_args, name: str | None = None, uc: bool | None = None):
     """
         This function reads a file and executes every command in the sel format,
         for more info check: TODO: Add Selenium Script DOcs line Here.
@@ -93,7 +99,7 @@ def selenium_script_shell(*args, name: None | str = None, uc: bool | None = None
 
         while True:
             try:
-                command = _parse_commands(input(">>> "), *args)[0]
+                command = _parse_command(input(">>> "), *args)
             except Exception as e:
                 print(e)
                 break
